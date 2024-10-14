3108131355dace604f17d74aedecd7dde22525b4
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-10-13 21:30:26 -0500

    finished lab

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,5 +1,30 @@
 package com.csc;
 
-public class Zipper {
+import java.util.ArrayList;
+import java.util.List;
+public class Zipper{
 
+    /*
+     * This function takes two lists of the same type and merges them together. If one list has
+     * more elements than the other, it will place all leftover elements at the end of the merged list in their
+     * original order. 
+     * @param list1, generic data type T
+     * @param list2, generic data type T
+     * @return merge, merged list of data type T
+     */
+    public <T> List<T> zip(List<T> list1, List<T> list2) {
+        List <T> merge = new ArrayList<>();
+        int maxLength = Math.max(list1.size(), list2.size());
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < list1.size()) {
+                merge.add(list1.get(i));
+            }
+            if (i < list2.size()) {
+                merge.add(list2.get(i));
+            }
+        }
+
+        return merge;
+    }
 }
--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -2,6 +2,10 @@ package com.csc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +18,57 @@ public class ZipperTest {
     zipper = new Zipper();
   }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+   @Test
+    void testZipEqualLengthLists() {
+        List<Integer> list1 = Arrays.asList(1, 3, 5);
+        List<Integer> list2 = Arrays.asList(2, 4, 6);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        assertEquals(expected, zipper.zip(list1, list2));
+    }
+
+    @Test
+    void testZipFirstListLonger() {
+        List<String> list1 = Arrays.asList("a", "b", "c", "d");
+        List<String> list2 = Arrays.asList("1", "2");
+        List<String> expected = Arrays.asList("a", "1", "b", "2", "c", "d");
+
+        assertEquals(expected, zipper.zip(list1, list2));
+    }
+
+    @Test
+    void testZipSecondListLonger() {
+        List<Character> list1 = Arrays.asList('x', 'y');
+        List<Character> list2 = Arrays.asList('1', '2', '3', '4');
+        List<Character> expected = Arrays.asList('x', '1', 'y', '2', '3', '4');
+
+        assertEquals(expected, zipper.zip(list1, list2));
+    }
+
+    @Test
+    void testZipWithEmptyFirstList() {
+        List<Double> list1 = Collections.emptyList();
+        List<Double> list2 = Arrays.asList(1.1, 2.2, 3.3);
+        List<Double> expected = Arrays.asList(1.1, 2.2, 3.3);
+
+        assertEquals(expected, zipper.zip(list1, list2));
+    }
+
+    @Test
+    void testZipWithEmptySecondList() {
+        List<Double> list1 = Arrays.asList(1.1, 2.2, 3.3);
+        List<Double> list2 = Collections.emptyList();
+        List<Double> expected = Arrays.asList(1.1, 2.2, 3.3);
+
+        assertEquals(expected, zipper.zip(list1, list2));
+    }
+
+    @Test
+    void testZipBothListsEmpty() {
+        List<Integer> list1 = Collections.emptyList();
+        List<Integer> list2 = Collections.emptyList();
+        List<Integer> expected = Collections.emptyList();
+
+        assertEquals(expected, zipper.zip(list1, list2));
+    }
 }
